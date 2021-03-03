15b6ade7d9934ea30d96a399f3c0f28691af3a80
Amir Reza Dalir <dalirnet@gmail.com>
2021-03-03 18:54:59 +0330

    start bash

--- src/extension.js
+++ src/extension.js
@@ -1,22 +1,49 @@
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
 const vscode = require('vscode')
 
-function activate(context) {
-    console.log('Congratulations, your extension "rtl-markdown" is now active!')
+// workbench
+const workbenchPath = path.resolve(require.main.path, ['vs', 'code', 'electron-browser', 'workbench', 'workbench.html'].join('/'))
+const workbenchFile = fs.existsSync(mainPath) ? fs.readFileSync(mainPath).toString() : String()
 
-    let rtl = vscode.commands.registerCommand('rtl-markdown.rtl', function () {
-        vscode.window.showInformationMessage('Hello World from RTL Markdown!')
-    })
+const bashPath = path.resolve(__dirname, 'bash.js')
+const stylePath = path.resolve(__dirname, 'style.css')
+const styleFile = fs.readFileSync(stylePath).toString().replace(/\s+/g, String())
 
-    let ltr = vscode.commands.registerCommand('rtl-markdown.ltr', function () {
-        vscode.window.showInformationMessage('Hello World from LTR Markdown!')
-    })
-
-    context.subscriptions.push(...[rtl, ltr])
+const init = () => {
+    try {
+        workbenchFile
+        // const command = [os.platform === 'linux' ? 'sudo' : '', 'node', writerPath]
+        // const terminal = vscode.window.createTerminal('RTL Markdown init')
+        // terminal.show()
+        // terminal.sendText(command.join(' '))
+        // terminal.sendText('sudo cat /usr/share/code/resources/app/out/vs/code/electron-browser/workbench/workbench.html')
+    } catch (e) {
+        console.log(e)
+    }
+    // fs.appendFileSync(mainPath, '--')
+    // console.log(mainFile.match(/\/*rtl-markdown-laga*\//))
 }
 
-function deactivate() {}
+const action = (from, to) => {
+    const workspaceEdit = new vscode.WorkspaceEdit()
+    if (vscode.window.activeTextEditor) {
+        init()
+        // workspaceEdit.renameFile(
+        //     vscode.Uri.file(vscode.window.activeTextEditor.document.uri.fsPath),
+        //     vscode.Uri.file(vscode.window.activeTextEditor.document.uri.fsPath.replace(new RegExp(from + '$', 'i'), to))
+        // )
+        // vscode.workspace.applyEdit(workspaceEdit)
+    }
+}
 
 module.exports = {
-    activate,
-    deactivate,
+    activate: (context) => {
+        context.subscriptions.push([
+            vscode.commands.registerCommand('rtl-markdown.rtl', () => action('.md', '.rtl.md')),
+            vscode.commands.registerCommand('rtl-markdown.ltr', () => action('.rtl.md', '.md')),
+        ])
+    },
+    deactivate: () => {},
 }
