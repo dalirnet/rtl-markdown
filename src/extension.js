e9f3ce53396eb73cf4992217239d7847f845e7d8
Amir Reza Dalir <dalirnet@gmail.com>
2021-03-04 15:51:05 +0330

    ready for test

--- src/extension.js
+++ src/extension.js
@@ -5,79 +5,101 @@ const crypto = require('crypto')
 const sudoPrompt = require('sudo-prompt')
 const vscode = require('vscode')
 
-const rootPath = path.resolve(require.main.path, '../')
-const mainStyleKey = 'vs/workbench/workbench.desktop.main.css'
-const mainStylePath = path.resolve(require.main.path, mainStyleKey)
-const injectStyleContent = fs.readFileSync(path.resolve(__dirname, 'style.css'))
-const injectFlag = /\/\*rtl\-markdown\-flag\*\//
-
-const prompt = () => {
-    sudoPrompt.exec('whoami', { name: 'RTL Markdown' }, function (error, stdout, stderr) {
-        if (error) {
-            console.log('error', error)
-        } else {
-            console.log('stdout: ' + stdout)
-        }
-    })
+const injection = {
+    command: os.platform() === 'win32' ? 'type' : 'cat',
+    flag: /\/\*rtl\-markdown\*\//,
+    target: 'vs/workbench/workbench.desktop.main.css',
+    subject: path.resolve(__dirname, 'style.css'),
+    status: () => !!workbench.file().match(injection.flag),
 }
-const checksum = () => {
-    const mainStyleContent = fs.existsSync(mainStylePath) ? fs.readFileSync(mainStylePath).toString() : String()
-    try {
-        let product = JSON.parse(fs.readFileSync(path.resolve(rootPath, 'product.json')))
-        product.checksums[mainStyleKey] = crypto.createHash('md5').update(mainStyleContent).digest('base64').replace(/=+$/, '')
 
-        console.log(product.checksums[mainStyleKey])
-        console.log(crypto.createHash('md5').update(mainStyleContent).digest('base64').replace(/=+$/, ''))
-    } catch (e) {
-        console.log(e)
-    }
+const workbench = {
+    path: {
+        get style() {
+            return path.resolve(require.main.path, injection.target)
+        },
+        get backup() {
+            return workbench.path.style + '.backup'
+        },
+    },
+    file() {
+        return fs.existsSync(workbench.path.style) ? fs.readFileSync(workbench.path.style).toString() : String()
+    },
 }
 
 const install = () => {
-    const mainStyleContent = fs.existsSync(mainStylePath) ? fs.readFileSync(mainStylePath).toString() : String()
-    if (!mainStyleContent.match(injectFlag)) {
-        try {
-            fs.appendFileSync(mainStylePath, '\n' + injectStyleContent)
-            vscode.window.showInformationMessage('RTL Markdown was installed successfully')
-        } catch (e) {
-            if (os.platform() === 'win32') {
-                vscode.window.showWarningMessage('Reopen the vscode with "run as administrator"')
-            } else {
-                vscode.window.showWarningMessage('Follow the terminal command to grant "sudo" access')
-                const terminal = vscode.window.createTerminal('RTL Markdown init')
-                terminal.sendText('sudo chown -R $(whoami) ' + rootPath)
-                terminal.show()
-            }
+    if (!injection.status()) {
+        const command = {
+            backup() {
+                return `${injection.command} ${workbench.path.style} >> ${workbench.path.backup}`
+            },
+            inject() {
+                return `${injection.command} ${injection.subject} >> ${workbench.path.style}`
+            },
         }
+        try {
+            sudoPrompt.exec([command.backup(), command.inject()].join(' && '), { name: 'RTL Markdown' }, (e) => {
+                if (e) {
+                    console.log(e)
+                    vscode.window.showErrorMessage('"RTL Markdown" cannot be installed!')
+                } else {
+                    vscode.window.showInformationMessage('"RTL Markdown" was installed successfully.', 'Restart').then(() => {
+                        vscode.commands.executeCommand('workbench.action.reloadWindow')
+                    })
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            vscode.window.showErrorMessage('"RTL Markdown" cannot be installed!')
+        }
+    } else {
+        vscode.window.showWarningMessage('"RTL Markdown" is already installed!')
+    }
+}
+
+const uninstall = () => {
+    if (injection.status()) {
+        const command = `${injection.command} ${workbench.path.backup} > ${workbench.path.style}`
+        try {
+            sudoPrompt.exec(command, { name: 'RTL Markdown' }, (e) => {
+                if (e) {
+                    console.log(e)
+                    vscode.window.showErrorMessage('"RTL Markdown" cannot be uninstalled!')
+                } else {
+                    vscode.window.showInformationMessage('"RTL Markdown" was uninstalled successfully.', 'Restart').then(() => {
+                        vscode.commands.executeCommand('workbench.action.reloadWindow')
+                    })
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            vscode.window.showErrorMessage('"RTL Markdown" cannot be uninstalled!')
+        }
+    } else {
+        vscode.window.showWarningMessage('"RTL Markdown" is already uninstalled!')
     }
 }
 
 const switchDirection = (from, to) => {
-    prompt()
-    // const mainStyleContent = fs.existsSync(mainStylePath) ? fs.readFileSync(mainStylePath).toString() : String()
-    // checksum()
-    // if (mainStyleContent.match(injectFlag)) {
-    //     const workspaceEdit = new vscode.WorkspaceEdit()
-    //     if (vscode.window.activeTextEditor) {
-    //         workspaceEdit.renameFile(
-    //             vscode.Uri.file(vscode.window.activeTextEditor.document.uri.fsPath),
-    //             vscode.Uri.file(vscode.window.activeTextEditor.document.uri.fsPath.replace(new RegExp(from + '$', 'i'), to))
-    //         )
-    //         vscode.workspace.applyEdit(workspaceEdit)
-    //     }
-    // } else {
-    //     vscode.window.showWarningMessage('RTL Markdown is not installed!', { title: 'Install', id: 'install' }).then(({ id }) => {
-    //         if (id === 'install') {
-    //             install()
-    //         }
-    //     })
-    // }
+    if (injection.status()) {
+        const workspaceEdit = new vscode.WorkspaceEdit()
+        if (vscode.window.activeTextEditor) {
+            workspaceEdit.renameFile(
+                vscode.Uri.file(vscode.window.activeTextEditor.document.uri.fsPath),
+                vscode.Uri.file(vscode.window.activeTextEditor.document.uri.fsPath.replace(new RegExp(from + '$', 'i'), to))
+            )
+            vscode.workspace.applyEdit(workspaceEdit)
+        }
+    } else {
+        vscode.window.showWarningMessage('"RTL Markdown" is not installed!', 'Install').then(() => install())
+    }
 }
 
 module.exports = {
     activate: (context) => {
         context.subscriptions.push([
             vscode.commands.registerCommand('rtl-markdown.install', () => install()),
+            vscode.commands.registerCommand('rtl-markdown.uninstall', () => uninstall()),
             vscode.commands.registerCommand('rtl-markdown.rtl', () => switchDirection('.md', '.rtl.md')),
             vscode.commands.registerCommand('rtl-markdown.ltr', () => switchDirection('.rtl.md', '.md')),
         ])
